8b7fd49f0fa65477c703f1b68ed4dcd3de0f9775
Cornelia Calin <corneliacalin@yahoo.com>
2021-07-25 12:57:04 +0300

    initial commit

---
+++ src/main/java/com/corneliacalin/sda/onlineanimalshop/OnlineanimalshopApplication.java
@@ -0,0 +1,13 @@
+package com.corneliacalin.sda.onlineanimalshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineanimalshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineanimalshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/corneliacalin/sda/onlineanimalshop/OnlineanimalshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corneliacalin.sda.onlineanimalshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineanimalshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
