dc01f366b4b98e4a27e4e1b01d9dc5be11fbbe6f
Cornelia Calin <corneliacalin@yahoo.com>
2021-07-25 13:35:01 +0300

    add database connection configuration

--- src/main/java/com/corneliacalin/sda/onlineanimalshop/OnlineAnimalShopApplication.java
+++ src/main/java/com/corneliacalin/sda/onlineanimalshop/OnlineAnimalShopApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OnlineanimalshopApplication {
+public class OnlineAnimalShopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OnlineanimalshopApplication.class, args);
+		SpringApplication.run(OnlineAnimalShopApplication.class, args);
 	}
 
 }
